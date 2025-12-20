94acb8fd7c7f1fb95b38e774140360fdc9b01138
Samuel Harris <samuel.matsuo.harris@gmail.com>
2025-12-20 13:24:40 +0000

    Removed data_fetcher

--- check_schema_keys.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.getcwd())
-from discount_analyst.shared.data_types import StockData
-
-schema = StockData.model_json_schema()
-properties = schema.get("properties", {}).keys()
-print("Properties in schema:", list(properties))
